094763dd0ea219d82920df085d97332eb522dd56
fordc02 <connor.ford@bbc.co.uk>
2022-09-02 13:13:01 +0100

    clean up argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,44 @@
-mod shade;
-mod encode;
 mod chunk;
+mod encode;
+mod shade;
 
-use crate::shade::Shade::{Mono, Greyscale};
 use crate::chunk::runs;
+use crate::shade::Shade::{Greyscale, Mono};
 use std::env;
 use std::fs;
 
+fn help() {
+    println!("usage:
+rle mono <inputfile>
+    monochrome image encoding
+rle grey <inputfile>
+    greyscale (hex shade) image encoding");
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let shade = match &(&args[1])[..] {
-        "m" => Mono,
-        "g" => Greyscale,
-        _ => panic!("usage: [m|g] inputfile")
-    };
-    let in_file_path = &args[2];
+    match args.len() {
+        3 => {
+            let shade = match &(&args[1])[..] {
+                "mono" => Mono,
+                "grey" => Greyscale,
+                _ => {
+                    eprintln!("error: first argument not mono or grey");
+                    help();
+                    return;
+                }
+            };
 
-    let contents = fs::read_to_string(in_file_path).expect("Should be able to read input file");
+            let in_file_path = &args[2];
 
-    println!("{}", runs(&contents, shade));
+            let contents =
+                fs::read_to_string(in_file_path).expect("could not read input file");
+
+            println!("{}", runs(&contents, shade));
+        }
+        _ => {
+            help();
+        }
+    }
 }
-
