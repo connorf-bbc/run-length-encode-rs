d1b41cd29bbd8273451579be6f9c035b7d559500
fordc02 <connor.ford@bbc.co.uk>
2022-09-02 10:28:29 +0100

    strip out other implementations and refactor the stack based approach

--- src/main.rs
+++ src/main.rs
@@ -2,95 +2,35 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn runs_basic(s: &str) -> String {
-    if s.is_empty() {
-        return "".to_string();
-    }
-
-    let n_black = s.chars().filter(|c| *c == '0').count();
-    let n_white = s.chars().filter(|c| *c == '1').count();
-
-    if n_white == 0 {
-        return n_black.to_string();
-    } else {
-        return format!("{:?},{:?}", n_black, n_white);
-    }
-}
-
-fn runs_foreach(s: &str) -> String {
-    let mut current_run: Option<(char, usize)> = None;
-    let mut acc: String = String::with_capacity(10);
-
-    s.chars().for_each(|c| match current_run {
-        None => {
-            if c == '1' {
-                acc.push_str("0,");
-            }
-            current_run = Some((c, 1));
-        }
-        Some((run_c, n_run_c)) => {
-            if c == run_c {
-                current_run = Some((run_c, n_run_c + 1));
-            } else {
-                acc.push_str(&n_run_c.to_string());
-                acc.push(',');
-                current_run = Some((c, 1));
-            }
-        }
-    });
-
-    match current_run {
-        None => {}
-        Some((_run_c, n_run_c)) => {
-            acc.push_str(&n_run_c.to_string());
-        }
-    }
-
-    return acc;
-}
-
-fn runs_fold(s: &str) -> String {
+fn runs(s: &str) -> String {
     return chunks(s)
         .iter()
         .map(|(_c, n)| n.to_string())
-        .collect();
+        .collect::<Vec<String>>()
+        .join(",");
 }
 
-// fold s into an list of runs (char, length)
 fn chunks(s: &str) -> Vec<(char, usize)> {
-    // can we push and pop off the end of vec instead of needing this?
-    // will that let us prevent the off by one issue
-    let mut current_run: Option<(char, usize)> = None;
+    let mut acc = Vec::<(char, usize)>::with_capacity(s.len());
 
-    return s
-        .chars()
-        .fold(
-            Vec::<(char, usize)>::with_capacity(s.len()),
-            |mut acc, c| {
-                match current_run {
-                    None => {
-                        if c == '1' {
-                            // b/w encoding always starts from black
-                            acc.push(('0', 0));
-                        }
-                        current_run = Some((c, 1));
-                    }
-                    Some((run_c, n_run_c)) => {
-                        if c == run_c {
-                            current_run = Some((run_c, n_run_c + 1));
-                        } else {
-                            acc.push((run_c, n_run_c));
-                            current_run = Some((c, 1));
-                        }
-                    }
-                }
-                acc
-            },
-        );
-}
-
-fn runs(s: &str) -> String {
-    return runs_fold(s);
+    s.chars().for_each(|c| match acc.pop() {
+        None => {
+            if c == '1' {
+                // b/w encoding always starts from black
+                acc.push(('0', 0));
+            }
+            acc.push((c, 1));
+        }
+        Some((run_c, n_run_c)) => {
+            if c == run_c {
+                acc.push((run_c, n_run_c + 1));
+            } else {
+                acc.push((run_c, n_run_c));
+                acc.push((c, 1));
+            }
+        }
+    });
+    return acc;
 }
 
 #[test]
